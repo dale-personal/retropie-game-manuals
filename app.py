9677fa20310651902cdd85f1d0847433329d8da8
Dale Koch <dalek@marvin.com>
2022-10-12 13:44:38 -0500

    no wait attempt

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ manualDir = sys.argv[1]
 @app.route('/<system>/<game>/manual', methods = ['GET'])
 def manual(system=None, game=None):
   subprocess.run(["wmctrl", "-c", "qpdfview"])
-  result = subprocess.run(["qpdfview", "--quiet", manualDir + '/' + system + '/' + '/' + game + '.pdf'], capture_output=True)
-  return 'Hello ' + system + ',' + game
+  pdfFile = manualDir + '/' + system + '/' + '/' + game + '.pdf'
+  result = subprocess.run([sys.executable, "qpdfview", "--quiet", pdfFile], capture_output=True)
+  return pdfFile + ', returnCode=' + result.returncode
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
