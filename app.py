c38e5c73d416e456536dee9b078992eff976854e
Dale Koch <dalek@marvin.com>
2022-10-12 12:47:29 -0500

    app params

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from distutils.log import debug
 from flask import Flask
 import subprocess
+import sys
 
 app = Flask(__name__)
+
+manualDir = sys.argv[1]
+isDebug = sys.argv[2]
+port = sys.argv[3]
+
 
 @app.route('/<system>/<game>/manual', methods = ['GET'])
 def manual(system=None, game=None):
   subprocess.run(["wmctrl", "-c", "qpdfview"])
-  subprocess.run(["qpdfview", "--quiet", "/media/pi/SANDISK250/manuals/" + system + '/' + '/' + game + '.pdf'])
+  subprocess.run(["qpdfview", "--quiet", manualDir + '/' + system + '/' + '/' + game + '.pdf'])
   return 'Hello ' + system + ',' + game
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=isDebug, port=port)
