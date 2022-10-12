5d14ac123b9ede1bd9631f5c7f9be105c1634177
Dale Koch <dalek@marvin.com>
2022-10-12 08:12:47 -0500

    wip

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/<system>/<game>/manual', methods = ['GET'])
 def manual(system=None, game=None):
-  result = subprocess.run(["wmctrl", "-c", "qpdfview"], capture_output=True, text=True)
-  return 'Hello ' + system + ',' + game + ', stdout=' + result.stdout + ', stderr=' + result.stderr
+  subprocess.run(["wmctrl", "-c", "qpdfview"])
+  subprocess.run(["qpdfview", "--quiet", "/media/pi/SANDISK250/manuals/" + system + '/' + '/' + game + '.pdf'])
+  return 'Hello ' + system + ',' + game
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
