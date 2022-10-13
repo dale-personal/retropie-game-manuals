4566b8213479ca36c1c52819e5b9698c0f387eb2
Dale Koch <dalek@marvin.com>
2022-10-13 13:09:14 -0500

    fuzzy match pdf files.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import subprocess
 import sys
+import difflib
+import os
 
 app = Flask(__name__)
 
@@ -9,9 +11,19 @@ manualDir = sys.argv[1]
 @app.route('/<system>/<game>/manual', methods = ['GET'])
 def manual(system=None, game=None):
   subprocess.Popen(["wmctrl", "-c", "qpdfview"])
-  pdfFile = manualDir + '/' + system + '/' + '/' + game + '.pdf'
-  subprocess.Popen(["qpdfview", "--quiet", pdfFile])
-  return pdfFile
+  possibilities = []
+  for file in os.listdir(manualDir + '/' + system + '/'):
+    if file.lower().endswith('.pdf'):
+      possibilities.append(file)
+
+  target = game + '.pdf'
+  matches = difflib.get_close_matches(target, possibilities, 1, 0.8)
+  if matches.count > 0 :
+    pdfFile = manualDir + '/' + system + '/' + matches[0]
+    subprocess.Popen(["qpdfview", "--quiet", pdfFile])
+    return 'Found: ' + pdfFile
+  else:
+    return 'Not Found: ' + manualDir + '/' + system + '/' + target
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
