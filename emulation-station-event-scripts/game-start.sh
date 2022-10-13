78fe7aa2ad62bb959207c247df0ec9857e55fff4
Dale Koch <dalek@marvin.com>
2022-10-12 21:44:14 -0500

    fix script

--- emulation-station-event-scripts/game-start.sh
+++ emulation-station-event-scripts/game-start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-CONSOLE="$1"
-ROMNAME=$(basename "$2")
+DIRNAME=$(dirname "$1")
+CONSOLE=$(basename $DIRNAME)
+ROMNAME="$2"
 
-URLENCODED_FILENAME="$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1].lower()))" "${ROMNAME%.*}")"
+URLENCODED_FILENAME="$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" "${ROMNAME}")"
 
-URL="http://raspberrypi:8000/$CONSOLE/$URLENCODED_FILENAME/manual"
+URL="http://raspberrypi.local:8000/$CONSOLE/$URLENCODED_FILENAME/manual"
 
-curl --silent "$URL" >> /dev/null 2>/dev/null &
+curl --silent "$URL" >> /dev/null 2>/dev/null
